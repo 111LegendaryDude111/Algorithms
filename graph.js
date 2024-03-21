1be14b4ca291f40a0f2a908fc075d47115ad6381
David Sukhashvili <davidsukhashvili@MacBook-Pro-David.local>
2024-03-21 18:39:03 +0300

    m

--- graph.js
+++ graph.js
@@ -11,30 +11,50 @@ function fetch(from) {
 }
 
 function getRoute(from, to) {
-  const rootTo = to;
-  const result = [];
-  let stack = [...fetch(from)];
+  let result = [from];
+  const stack = [...fetch(from)];
 
   while (stack.length > 0) {
-    let currentPoint = stack.pop();
-    let currentWay = fetch(currentPoint);
+    const currentWay = stack.pop();
+    result.push(currentWay);
+    const isFind = fetch(currentWay) && fetch(currentWay).includes(to);
 
-    if (currentWay && currentWay.includes(to)) {
-      result.push({ point: currentPoint, way: currentWay });
-      to = currentPoint;
-      stack.push(...fetch(from));
-    } else if (currentWay) {
-      stack.push(...currentWay);
+    if (isFind) {
+      result.push(to);
+      return result;
+    } else if (fetch(currentWay)) {
+      stack.push(...fetch(currentWay));
+    } else {
+      result = [from];
     }
   }
-
-  return result.length === 0
-    ? undefined
-    : [from, ...result.reverse().map((el) => el.point), rootTo];
+  return;
 }
-
 console.log(getRoute("A", "F")); // [ 'A', 'D', 'F' ]
 console.log(getRoute("A", "S")); // [ 'A', 'D', 'F', 'S' ]
 console.log(getRoute("D", "S")); //  [ 'D', 'F', 'S' ]
 console.log(getRoute("A", "Z")); //  [ 'A', 'B', 'Z' ]
 console.log(getRoute("B", "S")); //  undefined
+
+// function getRoute(from, to) {
+//   const rootTo = to;
+//   const result = [];
+//   let stack = [...fetch(from)];
+
+//   while (stack.length > 0) {
+//     let currentPoint = stack.pop();
+//     let currentWay = fetch(currentPoint);
+
+//     if (currentWay && currentWay.includes(to)) {
+//       result.push({ point: currentPoint, way: currentWay });
+//       to = currentPoint;
+//       stack.push(...fetch(from));
+//     } else if (currentWay) {
+//       stack.push(...currentWay);
+//     }
+//   }
+
+//   return result.length === 0
+//     ? undefined
+//     : [from, ...result.reverse().map((el) => el.point), rootTo];
+// }
