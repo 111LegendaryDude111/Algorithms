ae42f3627a670be0e9694694fda4c9d658a93a51
David Sukhashvili <davidsukhashvili@MacBook-Pro-David.local>
2024-04-27 17:03:58 +0300

    master

--- Graph/FindRoute.js
+++ Graph/FindRoute.js
@@ -1,5 +1,4 @@
 function fetch(from) {
-  // from - это точка
   const data = {
     A: ["B", "D"],
     B: ["C", "N", "Z"],
@@ -10,56 +9,45 @@ function fetch(from) {
   return data[from];
 }
 
-// function getRoute(from, to) {
-//   const visited = new Set();
-
-//   let stack = [from];
-//   let result = [];
-//   while (stack.length) {
-//     const element = stack.pop();
-//     const nextPoints = visited.has(element) ? false : fetch(element);
-
-//     visited.add(element);
-//     result.push(element);
-
-//     if (element === to) {
-//       return result;
-//     }
-
-//     if (nextPoints) {
-//       stack.push(...nextPoints);
-//     } else {
-//       result.pop();
-//     }
-//   }
-// }
-
 function getRoute(from, to, result = [], visited = new Set()) {
-  result.push(from);
-  if (from === to) {
+  if (visited.has(from)) {
     return;
   }
 
-  const way = fetch(from);
+  if (from === to) {
+    result.push(from);
 
-  if (way) {
-    way.forEach((el) => {
-      getRoute(el, to, result);
-    });
-  } else {
-    result.pop();
+    return result;
   }
 
-  return result;
+  result.push(from);
+  visited.add(from);
+
+  const route = fetch(from);
+
+  if (route) {
+    for (let index = 0; index < route.length; index++) {
+      const element = route[index];
+      const r = getRoute(element, to, result, visited);
+
+      if (r) {
+        return result;
+      } else {
+        result.pop();
+      }
+    }
+  }
+
+  return;
 }
 
 console.log(getRoute("A", "F"));
 // [ 'A', 'D', 'F' ]
 console.log(getRoute("A", "S"));
-// // [ 'A', 'D', 'F', 'S' ]
+// [ 'A', 'D', 'F', 'S' ]
 console.log(getRoute("D", "S"));
-// //  [ 'D', 'F', 'S' ]
+//  [ 'D', 'F', 'S' ]
 console.log(getRoute("A", "Z"));
-// //  [ 'A', 'B', 'Z' ]
+//  [ 'A', 'B', 'Z' ]
 console.log(getRoute("B", "S"));
 //  undefined
