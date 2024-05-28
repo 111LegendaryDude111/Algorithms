9a2f950bc1c33d7c0d9df26ee91acbe5d781cd86
David Sukhashvili <davidsukhashvili@MacBook-Pro-David.local>
2024-05-28 19:25:41 +0300

    m

---
+++ leetcode/two-sum.js
@@ -0,0 +1,18 @@
+var twoSum = function (nums, target) {
+  //cpu - O(n) mem - O(n)
+  const map = new Map();
+  for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+    const difference = target - element;
+
+    if (map.has(difference)) {
+      return [map.get(difference), index];
+    }
+
+    map.set(element, index);
+  }
+
+  return;
+};
+
+console.log(twoSum([3, 4, 5, 6], 7));
