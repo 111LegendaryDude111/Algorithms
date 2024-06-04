0425d8b6b701ceedeab4bc28c8ef32e8fed23ee5
David Sukhashvili <davidsukhashvili@MacBook-Pro-David.local>
2024-06-04 12:17:02 +0300

    m

--- leetcode/two-sum.js
+++ leetcode/two-sum.js
@@ -1,19 +1,47 @@
+/* 
+    Hashmap
+*/
+
+// var twoSum = function (nums, target) {
+//   //cpu - O(n) mem - O(n)
+//   const map = new Map();
+//   for (let index = 0; index < nums.length; index++) {
+//     const element = nums[index];
+//     const difference = target - element;
+
+//     if (map.has(difference)) {
+//       return [map.get(difference) + 1, index + 1];
+//     }
+
+//     map.set(element, index);
+//   }
+
+//   return;
+// };
+
+/* 
+    Two pointers
+*/
+
 var twoSum = function (nums, target) {
-  //cpu - O(n) mem - O(n)
-  const map = new Map();
+  let l = 0,
+    r = nums.length - 1;
+
   for (let index = 0; index < nums.length; index++) {
-    const element = nums[index];
-    const difference = target - element;
+    const leftEl = nums[l];
+    const rightEl = nums[r];
+    const sum = leftEl + rightEl;
 
-    if (map.has(difference)) {
-      return [map.get(difference) + 1, index + 1];
+    if (sum === target) {
+      return [l + 1, r + 1];
+    } else if (sum > target) {
+      r--;
+    } else {
+      l++;
     }
-
-    map.set(element, index);
   }
-
-  return;
 };
 
-console.log(twoSum([3, 4, 5, 6], 7));
+console.log(twoSum([1, 2, 3, 4], 3));
 console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 4, 5, 6], 7));
